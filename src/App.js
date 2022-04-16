aab3a5761194209b2756a71663c912a5775e5f42
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-16 16:53:20 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react'
+import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+import TablePagination from '@material-ui/core/TablePagination';
+import Edit from './edit'
+
 
 function App() {
+  
+const[page,setPage]=useState(1)
+const [rowsPerPage, setRowsPerPage] =useState(10);
+const[open,setOpen]=useState(false)
+const [users,setUsers]=useState()
+const[edititem,setEdititem]=useState()
+  useEffect(()=>{
+async function getdata(){
+  const data=await axios.get(`http://localhost:9000/user/getallusers/?page=${page}`)
+  console.log(data.data.users)
+setUsers(data.data.users)
+}
+getdata()
+  },[page])
+ 
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+const opendialog=(k)=>{
+setEdititem(k)
+setOpen(true)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+     <div className='tablecontainer'>
+<table className='table'>
+  <tr className='heading'>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Created At</th>
+    <th>Updated At</th>
+    <th>Action</th>
+  </tr>
+  {users?.map((u,index)=>
+  <tr className='rows'>
+    <td className='name'><span className='namehead'>
+      {u?.name?.charAt(0)}</span>{u.name}</td>
+    <td>{u.email}</td>
+    <td>{u?.createdat?.slice(0,16)}</td>
+    <td>{u?.updatedat?.slice(0,16)}</td>
+    <td>
+      <button className='view' onClick={()=>opendialog(u)}>View</button>
+      </td>
+  </tr>)}
+</table>
+<div className='rajesh'>
+      
+      <Pagination count={10} variant="outlined" shape="rounded" />
+      <TablePagination
+      component="div"
+      count={100}
+      page={page}
+      onPageChange={handleChangePage}
+      rowsPerPage={rowsPerPage}
+      onRowsPerPageChange={handleChangeRowsPerPage}
+    />
     </div>
+    <Edit open={open} setOpen={setOpen} edititem={edititem} setUsers={setUsers} page={page}/>
+</div>
+   </div>
+
   );
 }
 
---
+++ src/edit.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
+import { TextField } from '@material-ui/core';
+import { useState,useEffect } from 'react';
+import axios from 'axios'
+import { SettingsBackupRestoreSharp } from '@material-ui/icons';
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+export default function Edit({open,setOpen,edititem,setUsers,page}) {
+    const [email,setEmail]=useState()
+useEffect(()=>{
+setEmail(edititem?.email)
+},[edititem])
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleChange = async(editdata) => {
+    const d=await axios.post(`http://localhost:9000/user/edituser/${edititem._id}`,{email:email})
+    const data=await axios.get(`http://localhost:9000/user/getallusers/?page=${page}`)
+setUsers(data.data.users)
+    setOpen(false);
+  };
+
+  return (
+    <div style={{width:'40vw'}}>
+   
+      <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+        Edit Email
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+         {edititem?.name}
+          </Typography>
+          <Typography gutterBottom>
+        <TextField value={edititem?.email} onChange={(e)=>setEmail(e.target.value)}/>
+          </Typography>
+          <Typography gutterBottom>
+           {edititem?.createdat}
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleChange} color="primary">
+            Save changes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
